65207d8a5630f0d57fc199234c3bb5fe258d8e8b
dadovan <dadovan@gmail.com>
2015-04-12 23:43:15 -0500

    Fixed error in calc.  Now computes correct optimum.  Cleaned code.  Added unit tests.

--- FSharpBeginner/Program.fs
+++ FSharpBeginner/Program.fs
@@ -1,4 +1,4 @@
-﻿let data = [ (0.0, 1.0); (1.0, 2.0); (3.0, 2.0); (4.0, 3.0)];
+﻿open Microsoft.VisualStudio.TestTools.UnitTesting;
 
 let square x = x * x
 
@@ -6,29 +6,27 @@ let hyp t0 t1 x = t0 + (t1 * x)
 
 let cost t0 t1 x y = (hyp t0 t1 x) - y
 
-let costSquared cost = square cost
-
 let sumCost t0 t1 d =
     d
     |> List.map (fun (x,y) -> cost t0 t1 x y)
     |> List.sum
 
-let allCost t0 t1 d = (1.0 / (2.0 * (float data.Length))) * (sumCost t0 t1 d)
+let allCost t0 t1 (d : (float * float) list) = (1.0 / (2.0 * (float d.Length))) * (square (sumCost t0 t1 d))
 
-let j0Derivative t0 t1 d = (1.0 / (float data.Length)) / ( d |> List.map (fun (x,y) -> cost t0 t1 x y) |> List.sum )
-let j1Derivative t0 t1 d = (1.0 / (float data.Length)) / ( d |> List.map (fun (x,y) -> (cost t0 t1 x y) * x) |> List.sum )
+let j0Derivative t0 t1 (d : (float * float) list) = (1.0 / (float d.Length)) * ( d |> List.map (fun (x,y) -> cost t0 t1 x y) |> List.sum )
+let j1Derivative t0 t1 (d : (float * float) list) = (1.0 / (float d.Length)) * ( d |> List.map (fun (x,y) -> (cost t0 t1 x y) * x) |> List.sum )
 
-let oneGD t0 t1 alpha d =
+let oneDescent t0 t1 alpha d =
     let temp0 = t0 - alpha * (j0Derivative t0 t1 d)
     let temp1 = t1 - alpha * (j1Derivative t0 t1 d)
     [temp0; temp1]
 
 let rec fullGD (t : float list) a d i currentAllCost =
-    let newt = oneGD t.[0] t.[1] a d
+    let newt = oneDescent t.[0] t.[1] a d
     let newAllCost = allCost (newt.[0]) (newt.[1]) d
-    if (i % 100) = 0 then
-        printfn "%i %f %f %f" i newt.[0] newt.[1] newAllCost
     let i = i + 1
+    if ((i % 1000) = 0) || (newAllCost >= currentAllCost) then
+        printfn "Iteration: %i, t0: %f, t1: %f, cost: %f" i newt.[0] newt.[1] newAllCost
     if (newAllCost >= currentAllCost) then
         newt
     else
@@ -36,26 +34,18 @@ let rec fullGD (t : float list) a d i currentAllCost =
 
 [<EntryPoint>]
 let main argv = 
-    // printfn "%A" argv
-
+    let data = [ (0.0, 1.0); (1.0, 2.0); (3.0, 2.0); (4.0, 3.0)];
     printfn "Data: %A" data
 
-    // Best values/cost
-    let t0 = 1.2
-    let t1 = 0.4
-    let c = cost t0 t1 0.0 1.0
-    printfn "%f %f %f" t0 t1 c
-    let x1 = sumCost t0 t1 data
-    printfn "%A" x1
-    let x2 = allCost t0 t1 data
-    printfn "%A" x2
+    // Tests
+    let t0 = 1.87
+    let t1 = 0.237
+    Assert.AreEqual(0.107, (cost t0 t1 1.0 2.0), 0.001)
+    Assert.AreEqual(0.344, (j0Derivative t0 t1 data), 0.001)
 
     // Gradient descent to find approx global optimum
-    let alpha = 0.001
-    let t0 = 4.0
-    let t1 = 1.2
-    let thetas = [t0; t1]
-    let i = 1
-    let newt = fullGD thetas alpha data i (float System.Int32.MaxValue)
+    let newT = fullGD [4.0; 1.2] 0.01 data 1 (float System.Int32.MaxValue)
+    Assert.AreEqual(1.2, newT.[0], 0.0001);
+    Assert.AreEqual(0.4, newT.[1], 0.0001);
 
     0 // return an integer exit code
